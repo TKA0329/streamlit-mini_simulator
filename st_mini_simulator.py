1a82e9089efe66046840373534de6178af5ec2a3
TKA <tka0329ann@gmail.com>
2025-06-17 21:52:55 +0800

    Add bar graph and carbon dioxide emissions

--- st_mini_simulator.py
+++ st_mini_simulator.py
@@ -4,11 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd 
 import io 
+import matplotlib
 from matplotlib.animation import FuncAnimation
+import matplotlib.cm as cm
+import matplotlib.colors as mcolors
 import csv
 
 st.title("Mini Reaction Kinetics and Heat Transfer Simulator")
-type_of_cal = st.selectbox("Heat Transfer or Reaction Kinetics:", ["Heat Transfer", "Reaction Kinetics"])
+type_of_cal = st.selectbox("Heat Transfer or Reaction Kinetics:", ["Heat Transfer", "Reaction Kinetics", "GHG Impact Simulator"])
 
 def type_of_calculation():
     if type_of_cal == "Reaction Kinetics":
@@ -316,7 +319,7 @@ def type_of_calculation():
                     common_substance = st.multiselect("Choose a material:", compound_names) #returns a list
                     st.subheader("Graphs")
                     if not common_substance:
-                        st.warning("Please select at least one item from the table!")
+                        st.warning("To display the graph, please select at least one item from the table!")
                         return
                     for substance in common_substance:
                         for item in table:
@@ -487,8 +490,8 @@ def type_of_calculation():
 
         if calculation == "6. Conduction (Fourier’s law) (graphing available!)":
             ask_user = st.selectbox("(1) Temperature against Distance graph of different materials or (2) calculate rate of energy transfer? Please type 1 or 2.", ["Temperature against Distance graph of different materials", "Calculate rate of energy transfer"])
-            st.subheader("Calculation using the Fourier's Law")
             if ask_user == "Calculate rate of energy transfer":
+                st.subheader("Calculation using the Fourier's Law")
                 T_high = st.number_input("Temperature of high-temperature region in K:", min_value = 1e-6) #unit: K
                 T_low = st.number_input("Temperature of low-temperature region in K:", min_value=1e-6) #unit: K
                 if T_high < 1e-6 or T_low < 1e-6:
@@ -519,7 +522,7 @@ def type_of_calculation():
                 common_substance = st.multiselect(f"Enter the name of substance:", material_names) #returns list
                 st.subheader("Graph")
                 if not common_substance:
-                    st.warning("Please select at least one item from the table!")
+                    st.warning("To display the graph, please select at least one item from the table!")
                     return
                 for substance in common_substance: #to ensure substances e.g. Aluminium and Aluminium nitride don't get mixed up
                     for item in table:
@@ -585,7 +588,7 @@ def type_of_calculation():
                 common_substance = st.multiselect(f"Enter the name of substance:", material_names) #returns list
                 st.subheader("Graph")
                 if not common_substance:
-                    st.warning("Please select at least one item from the table!")
+                    st.warning("To display the graph, please select at least one item from the table!")
                     return
                 table_substance = {}
                 for substance in common_substance:
@@ -623,6 +626,100 @@ def type_of_calculation():
             delta_T = st.number_input("Temperature difference (ΔT):", min_value = 0.0) #unit: K
             rate_of_heat_transfer = h * A *delta_T
             st.info(f"Rate of energy transfer = {rate_of_heat_transfer} W")
+    
+    if type_of_cal == "GHG Impact Simulator":
+        cal_or_bar = st.selectbox("Calculate Amount of Carbon Dioxide Emitted or Compare Carbon Emissions (Bar Graph)?", ["Compare Carbon Emissions (Bar Graph)", "Calculate Amount of Carbon Dioxide Emitted"])
+        table = [] #preparing a list for appending
+        with open("list_of_fuels.csv","r", encoding = "utf-8") as file: #open csv
+            reader = csv.DictReader(file)
+            for row in reader:
+                table.append({"Fuel": row["Fuel"],
+                                "Energy Content (MJ/kg)":row["Energy Content (MJ/kg)"], "Carbon Emissions (gCO2/MJ)":row["Carbon Emissions (gCO2/MJ)"]})
+        st.subheader("Table: Material, Energy Content and Carbon Emissions")
+        df = pd.DataFrame(table)
+        st.dataframe(df)
+        st.write("*Assumes green hydrogen. No direct CO₂ emissions upon use.")
+        st.write("Note: ☘️ means it is a renewable energy!")
+        
+        if cal_or_bar == "Calculate Amount of Carbon Dioxide Emitted":
+            st.subheader("Amount of Carbon Dioxide Emitted Calculator")
+            amount_of_energy = st.number_input("Based on the table above or your own value, energy content of the fuel (MJ/kg):", min_value = 0.0)
+            mass = st.number_input("Mass of the fuel consumed (kg): ", min_value = 0.0)
+            carbon_emissions_cal = st.number_input("Based on the table above or your own value, carbon emissions (gCO2/MJ): ", min_value = 0.0)
+            if amount_of_energy == 0 or mass == 0:
+                st.warning("Energy content and Mass cannot be 0!")
+                return
+            carbon_emitted = amount_of_energy * mass * carbon_emissions_cal
+            st.info(f"{mass}kg of the fuel emitted {round(carbon_emitted/1000,3)}kg of CO₂.")
+            st.info(f"🌳 To offset {round(carbon_emitted/1000,3)}kg of CO₂, you would need approximately {round(carbon_emitted/1000/26.635)} trees!")
+
+        if cal_or_bar == "Compare Carbon Emissions (Bar Graph)":
+            table_substance = {}
+            material_names = (name["Fuel"] for name in table)
+            common_substance = st.multiselect(f"Enter the name of substance:", material_names) #returns list
+            st.subheader("Graph")
+            
+            if not common_substance:
+                st.warning("To display the graph, please select at least one item from the table!")
+                return
+            for substance in common_substance: 
+                for item in table:
+                    if substance == item["Fuel"]:
+                        energy_content = float(item["Energy Content (MJ/kg)"])
+                        carbon_emissions = float(item["Carbon Emissions (gCO2/MJ)"])
+                        table_substance[item["Fuel"]] = {"Energy Content (MJ/kg)": energy_content, "Carbon Emissions (gCO2/MJ)": carbon_emissions}
+
+            fig1, ax1 = plt.subplots(figsize = (12,6))
+            renewable_energies = ["Hydrogen* (HHV)☘️", "Hydrogen* (LHV)☘️", "Vegetable Oil☘️", "Biodiesel☘️"] 
+
+            fuel_names = list(table_substance.keys())
+            colors = matplotlib.colormaps.get_cmap("tab20", len(fuel_names))
+            fuel_color_map = {fuel: colors(i) for i, fuel in enumerate(fuel_names)}
+
+            val_based = {k: v for k, v in sorted(table_substance.items(), key=lambda item:item[1]["Energy Content (MJ/kg)"])}
+            for fuel, energy_con in val_based.items():
+                Fuel = [fuel]
+                Energy = [energy_con["Energy Content (MJ/kg)"]]
+                energy_val = energy_con["Energy Content (MJ/kg)"]
+                color = fuel_color_map[fuel]
+                bars1 = ax1.bar(Fuel, Energy, width = 0.3, color=color)
+                ax1.bar_label(bars1, label_type = "edge", color = "blue")
+            
+                if fuel in renewable_energies: #cannot use a list in ax.text!
+                    ax1.text(fuel, energy_val + 5, "☘️", ha = "center", fontsize = 20)
+            
+            #graph's info
+            ax1.set_xlabel("Fuel")
+            ax1.set_ylabel("Energy Content (MJ/kg)")
+            ax1.tick_params(axis = "x", labelrotation=45)
+            plot_title = st.text_input("Graph's title: ", key = "F")
+            ax1.set_title(f"{plot_title}")
+            st.pyplot(fig1)
+            
+            fig2, ax2 = plt.subplots(figsize = (12,6))
+            
+            #table substance contains dicts within a dict, you need to specify which item in the 2nd dict to access 
+            val_based = {k: v for k, v in sorted(table_substance.items(), key=lambda item:item[1]["Carbon Emissions (gCO2/MJ)"])}
+            
+            for fuel, energy_con in val_based.items():
+                Fuel = [fuel] #fuel gets the key, energy_con gets the value
+                Energy2 = [energy_con["Carbon Emissions (gCO2/MJ)"]]
+                energy_2 = energy_con["Carbon Emissions (gCO2/MJ)"]
+                color = fuel_color_map[fuel]
+                bars2 = ax2.bar(Fuel, Energy2, width = 0.3, color=color)
+                ax2.bar_label(bars2, label_type = "edge", color = "blue")
+
+                if fuel in renewable_energies: #cannot use a list in ax.text!
+                    ax2.text(fuel, energy_2 + 5, "☘️", ha = "center", fontsize = 20)
+            
+            #graph's info
+            ax2.set_xlabel("Fuel")
+            ax2.set_ylabel("Carbon Emissions (gCO2/MJ)")
+            ax2.tick_params(axis = "x", labelrotation=45)
+            plot_title = st.text_input("Graph's title: ", key = "C")
+            ax2.set_title(f"{plot_title}")
+            st.pyplot(fig2)
+
 def main():
     type_of_calculation()
 
